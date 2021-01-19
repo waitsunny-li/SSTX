f40fba501dd4518e7c67083f28f7a179ad778bcf
Weilong Li <weilongli@WeilongdeMacBook-Pro.local>
2021-01-19 22:13:28 +0800

    完善我的基本信息和更改手机号

--- pages/basicinfo/basicinfo.js
+++ pages/basicinfo/basicinfo.js
@@ -2,30 +2,100 @@
  * @Author: liweilong
  * @Date: 2021-01-12 16:08:47
  */
+
+import { request } from "../../request/index";
+import {
+  chooseOneImg,
+  uploadFile
+} from '../../utils/asyncWx'
 // pages/basicinfo/basicinfo.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    // 获取的位置信息的列下标
+    procityIndex: [0, 0],
+    provinceName: '北京',
+    cityName: '北京市',
+    objectCityArray: [],
+
     times: '发送验证码',
     isSend: false,
     timeIndex: 0,
+    // form field
     sexList: ['女', '男', '未选择'],
     sexIndex: 2,
+    nickname: '',
+    mobile: '17333283006',
+    avatar: '',
+    uploadAvatar: '',
+
+    poptype: '',
+    popmsg: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取缓存中的当前位置信息，如果有
+    let procityObj = wx.getStorageSync('procityObj')
+    if (procityObj) {
+      this.setData({
+        procityIndex: procityObj.procityIndex,
+        provinceName: procityObj.procityName[0],
+        cityName: procityObj.procityName[1],
+        objectCityArray: procityObj.objectCityArray
+      })
+    }
+    
   },
 
-  // send code 
-  handleSendCode(e) {
-    this.sendCode(60)
+  onShow: function() {
+    // 获取缓存中的VIP信息
+    const vipUserInfo = wx.getStorageSync('vipUserInfo');
+    this.setData({
+      nickname: vipUserInfo.nickname,
+      // mobile: vipUserInfo.mobile,
+      sexIndex: vipUserInfo.gender,
+      avatar: vipUserInfo.avatar
+    })
+  },
+
+  // upload avatar
+  async handleUploadAvatarTap(e) {
+    let r
+    try {
+      r = await chooseOneImg({
+        sourceType: ['album']
+      })
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+    let tempFilePaths = r.tempFilePaths
+    let result = await uploadFile({
+      filePath: tempFilePaths[0],
+      name: 'file',
+      header: {
+        'token': wx.getStorageSync('token'),
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    let data = JSON.parse(result.data)
+    console.log(data);
+    if (data.code == 1) {
+      this.setData({
+        avatar: tempFilePaths[0],
+        uploadAvatar: data.data.url
+      })
+    } else {
+      this.setData({
+        poptype: 'error',
+        popmsg: data.data.msg
+      })
+    }
   },
 
   // sex change
@@ -34,6 +104,35 @@ Page({
     this.setData({
       sexIndex: e.detail.value
     })
+  },
+
+  // change location
+  handleNeedLocation(e) {
+    const [provinceName, cityName] = e.detail.address
+    this.setData({
+      provinceName,
+      cityName
+    })
+  },
+
+  // send code 
+  async handleSendCode(e) {
+    const r = await request({
+      url: '/sms/send',
+      method: 'post',
+      data: {
+        mobile: this.data.mobile,
+        event: 'realname'
+      }
+    })
+    if (r.code == 1) {
+      this.sendCode(60)
+    } else {
+      this.setData({
+        poptype: 'error',
+        popmsg: '手机号格式错误！'
+      })
+    }
   },
 
   // func send
@@ -65,6 +164,32 @@ Page({
     this.setData({
       timeIndex
     })
+  },
+
+  // form submit
+  async handleFormSubmit(e) {
+    const {value} = e.detail
+    value.address = this.data.provinceName + '/' + this.data.cityName
+    console.log(value);
+    const r = await request({
+      url: '',
+      method: 'post',
+      data: value
+    })
+    if (r.code == 1) {
+      wx.showToast({
+        title: '保存成功',
+        icon: 'success',
+        success: () => {
+          console.log('回调');
+        }
+      })
+    } else {
+      this.setData({
+        poptype: 'error',
+        popmsg: r.data.msg
+      })
+    }
   }
 
 })
--- pages/modifytel/modifytel.js
+++ pages/modifytel/modifytel.js
@@ -2,6 +2,9 @@
  * @Author: liweilong
  * @Date: 2021-01-12 12:12:58
  */
+
+import { request } from "../../request/index"
+
 // pages/modifytel/modifytel.js
 Page({
 
@@ -11,19 +14,53 @@ Page({
   data: {
     times: '发送验证码',
     isSend: false,
-    timeIndex: 0
+    timeIndex: 0,
+    mobile: '',
+    newmobile: '',
+
+    poptype: '',
+    popmsg: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let {mobile} = options
+    let start = mobile.substr(0, 3)
+    let end = mobile.substr(mobile.length - 4)
+    let newmobile = start + '****' + end
+    this.setData({
+      mobile: newmobile
+    })
   },
 
+  // new mobile blur
+  handleNewMBlur(e) {
+    const {value} = e.detail
+    this.setData({
+      newmobile: value
+    })
+  },
   // send code 
-  handleSendCode(e) {
-    this.sendCode(60)
+  async handleSendCode(e) {
+    let newmobile = this.data.newmobile
+    if ((/^1[34578]\d{9}$/.test(newmobile))) {
+      this.sendCode(60)
+      const r = await request({
+        url: '/sms/send',
+        method: 'post',
+        data: {
+          mobile,
+          event: 'realname'
+        }
+      })
+    } else {
+      this.setData({
+        poptype: 'error',
+        popmsg: '手机号格式错误！'
+      })
+    }
   },
 
   // func send
@@ -55,5 +92,39 @@ Page({
     this.setData({
       timeIndex
     })
+  },
+
+  // form submit
+  async handleFormSubmit(e) {
+    const {value} = e.detail
+    console.log(value);
+    const r = await request({
+      url: '',
+      method: 'post',
+      data: value
+    })
+    if (r.code == 1) {
+      wx.showToast({
+        title: '修改成功！',
+        icon: 'success',
+        duration: 1500,
+        mask: false,
+        success: (result) => {
+          console.log('jjjj');
+          let vipUserInfo = wx.getStorageSync('vipUserInfo')
+          vipUserInfo.mobile = value.newmobile
+          wx.setStorageSync('vipUserInfo', vipUserInfo);
+            
+          wx.navigateBack({
+            delta: 1
+          })
+        },
+      })
+    } else {
+      this.setData({
+        poptype: 'error',
+        popmsg: r.data.data.msg
+      })
+    }
   }
 })
